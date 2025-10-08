f3e1528de78aaddb800f9f7d04ca9809dba55de7
Anatoly <yatskovanatoly@gmail.com>
2025-10-08 17:32:53 +0300

    fix(site): minor waves adjustment

--- site/components/Waves.tsx
+++ site/components/Waves.tsx
@@ -17,7 +17,7 @@ const WaveAnimation: FC<PropsWithChildren & { playing: boolean }> = ({
 				const nextIndex = (waves.indexOf(prev) + 1) % waves.length
 				return waves[nextIndex]
 			})
-		}, 2000)
+		}, 1000)
 		return () => clearInterval(interval)
 	}, [playing])
 
@@ -32,16 +32,16 @@ const WaveAnimation: FC<PropsWithChildren & { playing: boolean }> = ({
 }
 
 const WavesLeft: FC<{ src: string[] }> = ({ src }) => (
-	<div className='absolute right-full h-1/2 w-1/2 top-2/5 flex gap-2 sm:gap-6 max-sm:hidden'>
+	<div className='absolute right-full h-1/3 w-1/3 top-1/2 flex gap-2 sm:gap-6 max-sm:hidden'>
 		<div
 			className={`animate-fadeInOut relative opacity-0 h-full w-1/4`}
-			style={{ animationDelay: '500ms' }}
+			style={{ animationDelay: '200ms' }}
 		>
 			<Image src={src[0]} alt='wave-l-1' fill sizes='100vw' />
 		</div>
 		<div
 			className={`animate-fadeInOut relative opacity-0 top-1/5 h-2/3 w-1/6`}
-			style={{ animationDelay: '250ms' }}
+			style={{ animationDelay: '100ms' }}
 		>
 			<Image src={src[1]} alt='wave-l-2' fill sizes='100vw' />
 		</div>
@@ -49,13 +49,13 @@ const WavesLeft: FC<{ src: string[] }> = ({ src }) => (
 			className={`animate-fadeInOut relative opacity-0 top-2/5 h-2/5 w-1/8`}
 			style={{ animationDelay: '0ms' }}
 		>
-			<Image src={src[2]} alt='wave-l-3' fill sizes='100vw' />
+			<Image src={src[2]} alt='wave-l-3' fill sizes='100vh' />
 		</div>
 	</div>
 )
 
 const WavesRight: FC<{ src: string[] }> = ({ src }) => (
-	<div className='absolute -right-3/5 h-1/2 w-1/2 top-2/5 flex gap-2 sm:gap-6 max-sm:hidden'>
+	<div className='absolute -right-2/5 h-1/3 w-1/3 top-1/2 flex gap-2 sm:gap-6 max-sm:hidden'>
 		<div
 			className={`animate-fadeInOut relative opacity-0 top-2/5 h-2/5 w-1/8`}
 			style={{ animationDelay: '0ms' }}
@@ -64,13 +64,13 @@ const WavesRight: FC<{ src: string[] }> = ({ src }) => (
 		</div>
 		<div
 			className={`animate-fadeInOut relative opacity-0 top-1/5 h-2/3 w-1/6`}
-			style={{ animationDelay: '250ms' }}
+			style={{ animationDelay: '100ms' }}
 		>
 			<Image src={src[1]} alt='wave-r-2' fill />
 		</div>
 		<div
 			className={`animate-fadeInOut relative opacity-0 h-full w-1/4`}
-			style={{ animationDelay: '500ms' }}
+			style={{ animationDelay: '200ms' }}
 		>
 			<Image src={src[2]} alt='wave-r-3' fill />
 		</div>
