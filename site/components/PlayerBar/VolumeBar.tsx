cb41b02b95f9d10d59fb1dd3a6b4c477ad1dda6f
Anatoly <yatskovanatoly@gmail.com>
2025-10-10 03:43:15 +0300

    fix(site): volume bar accent in dark theme

--- site/components/PlayerBar/VolumeBar.tsx
+++ site/components/PlayerBar/VolumeBar.tsx
@@ -24,7 +24,7 @@ const VolumeBar = () => {
 					style={{ writingMode: 'vertical-lr', direction: 'rtl' }}
 					{...commonProps}
 					onChange={(e) => setVolume(parseFloat(e.target.value))}
-					className='volume-slider transition-all duration-700 absolute bottom-full left-0 right-0 accent-black'
+					className='volume-slider transition-all duration-700 absolute bottom-full left-0 right-0 accent-black dark:accent-white'
 				/>
 			)}
 			<button {...commonProps} onClick={() => setVolume(volume > 0 ? 0 : 1)}>
