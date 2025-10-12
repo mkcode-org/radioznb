4cd5be1ed33281b8e4529ff426f0c180a409d828
sukalov <matvey10@gmail.com>
2025-10-12 10:03:32 +0300

    feat: about page

--- site/app/about/page.tsx
+++ site/app/about/page.tsx
@@ -1,10 +1,36 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import Image from "next/image";
+import Link from "next/link";
 
-const Page = () => (
-	<Link href='/' className='w-fit'>
-		<Image src='/assets/logo.png' height={64} width={64} alt='logo' />
-	</Link>
-)
+const Page = async () => {
+  const request = await fetch(
+    "https://server.radioznb.ru/api/station/radioznb-live",
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_RADIOZNB_API_KEY}`,
+      },
+    }
+  );
+  const data = await request.json();
+  console.log(data);
+  return (
+    <div>
+      <Link href="/" className="w-fit">
+        <Image src="/assets/logo.png" height={64} width={64} alt="logo" />
+      </Link>
+      <p className="sm:p-12 p-4">{data.description}</p>
+      <div className="flex gap-4 justify-center">
+        <Link href="https://t.me/radi0ZnB">
+          <Image
+            className="relative size-24 hover:scale-105 transition-all"
+            width={12}
+            height={12}
+            src="/assets/tg.png"
+            alt="telegram"
+          />
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default Page
+export default Page;
